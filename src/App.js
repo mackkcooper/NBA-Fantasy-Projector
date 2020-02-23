f0492abcb461475c19b02ae6dde9de0f53121a2c
santitobon9 <43623235+santitobon9@users.noreply.github.com>
2020-02-22 16:24:29 -0800

    Modularize the js code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,8 @@ const $ = window.$;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="jumbotron">
-          <h1 className="display-1">NBA Fantasy Projector</h1>
-        </div>
-      </header>
-      <section className="container" id="player-search">
-        <div className="row form-group">
-          <div className="col-12 label">
-            <label>Enter players to add to your roster:</label>
-          </div>
-          <div className="col-12 col-sm-8 col-lg-9">
-            <input className="form-control" id="player-search-bar" type="text" placeholder="Stephen Curry"></input>
-          </div>
-          <div className="col-12 col-sm-4 col-lg-3">
-            <button className="btn form-control" type="button" onClick={AddPlayer}>Add Player</button>
-          </div>
-        </div>
-      </section>
+      {site_header}
+      {player_search}
     </div>
   );
 }
@@ -30,5 +14,32 @@ function AddPlayer() {
   console.log("Adding player to roster...");
   console.log("player entered: " + $("#player-search-bar").val());
 }
+
+// Site Header
+const site_header = (
+  <header className="App-header">
+    <div className="jumbotron">
+      <h1 className="display-1">NBA Fantasy Projector</h1>
+    </div>
+  </header>
+);
+
+// Player Search
+const player_search = (
+  <section className="container" id="player-search">
+    <div className="row form-group">
+      <div className="col-12 label">
+        <label>Enter players to add to your roster:</label>
+      </div>
+      <div className="col-12 col-sm-8 col-lg-9">
+        <input className="form-control" id="player-search-bar" type="text" placeholder="Stephen Curry"></input>
+      </div>
+      <div className="col-12 col-sm-4 col-lg-3">
+        <button className="btn form-control" type="button" onClick={AddPlayer}>Add Player</button>
+      </div>
+    </div>
+  </section>
+);
+
 
 export default App;
