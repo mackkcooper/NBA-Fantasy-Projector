c7451703d332e8f2dc13221b74088d74d5956e29
santitobon9 <43623235+santitobon9@users.noreply.github.com>
2020-03-13 13:48:22 -0700

    added comments to NBA-api

--- src/NBA-api.js
+++ src/NBA-api.js
@@ -7,9 +7,12 @@ function get_player (player_name) {
 }
 
 // get team name by team id
+// city is the city of the team that player plays for
+// name is the name of the team that player plays for
 function get_team_name(team_id, player_obj) {
     NBA.stats.teamInfoCommon({TeamID: team_id}).then(
         result => {
+            // city is the city the team of the player plays for
             var city = result.teamInfoCommon[0].teamCity;
             var name = result.teamInfoCommon[0].teamName;
             player_obj["team_name"] = city + " " + name;
@@ -17,6 +20,9 @@ function get_team_name(team_id, player_obj) {
     );
 }
 
+// with the player id, searchs for the stats in api and then passes
+// json obj into the function in the argument that will then insert
+// the stats into a new json object  
 function get_player_stats (player_id, player_obj, func) {
     NBA.stats.playerSplits({PlayerID: player_id})
     .then(players => 
@@ -27,7 +33,11 @@ function get_player_stats (player_id, player_obj, func) {
     });
 }
 
-// returns a json obj with all of the information of a player
+// creates a player object to insert all information of a player
+// into, then adds basic information from findplayer into the player
+// object. Calls helper functions to retrieve the remaining information
+// to be stored in the player object. Then returns the player json
+// object to be stored in a map later
 function retrieve_player (player_name, func) {
     var curr_player = NBA.findPlayer(player_name);
     var player_id = curr_player.playerId;
@@ -40,12 +50,16 @@ function retrieve_player (player_name, func) {
         "team_id"    : team_id,
     };
 
+//  helper functions
     get_team_name (team_id, player);
     get_positions(player_id, player)
     get_player_stats (player_id, player, insert_stats);
     func(player);
 }
 
+// This function takes in the json object from the api as source
+// and the new player json object the destination and copies over
+// the information from the source into the destination
 function insert_stats (src_player, dest_player) {
     dest_player["mins"] = src_player.overallPlayerDashboard[0].min;
     dest_player["fg_pct"] = src_player.overallPlayerDashboard[0].fgPct;
@@ -59,7 +73,9 @@ function insert_stats (src_player, dest_player) {
     dest_player["pts"] = src_player.overallPlayerDashboard[0].pts;
 }
 
-// retrieves the positions of the player
+// with the player id, searchs for the player's position in api and
+// then inserts the postion into the player json object that will
+// be used later in the map
 function get_positions (player_id, player_obj) {
     NBA.stats.playerInfo({PlayerID: player_id})
         .then(player => {
